740fa7a029f92ee7a22493c63ee841a460a98c9f
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-09-29 23:38:09 +0530

    added typesafety

--- backend/middleware/typeCheck/creadentialTypeCheck.js
+++ backend/middleware/typeCheck/creadentialTypeCheck.js
@@ -1,6 +1,28 @@
-const z = require('zod')
+const z = require("zod");
 
-const userScheama = z.object ({
-      username: z.string(),
-      email: z.string().email()
-})
+const userSchema = z.object({
+  userName: z
+    .string()
+    .min(1, { message: "Username is required." })
+    .max(15, { message: "Username must be 15 characters or less." }),
+  userEmail: z.string().email({ message: "Invalid email address." }),
+  userPassword: z
+    .string()
+    .min(5, { message: "Password must be at least 5 characters long." }),
+});
+
+function creadentialTypeCheck(req, res, next) {
+  const userCreadentials = req.body;
+  const result = userSchema.safeParse(userCreadentials);
+
+  if (!result.success) {
+    res.status(400).json({
+      success:false,
+      error:result.error.format()
+    });
+    return
+  }
+  next()
+}
+
+module.exports = creadentialTypeCheck;
