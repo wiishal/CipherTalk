b8b40f7dbc6c13f90d3a840ec53e3d093da41088
vishal-Nanaware <vishalnanaware7850@gmail.com>
2024-10-24 23:35:21 +0530

    Seperate user-functionality

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -3,15 +3,87 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 async function createUser(userName, email, password) {
-  const newUser = await prisma.user.create({
-    data: {
-      username: userName,
-      email: email,
-      password: password, 
-    },
-  });
+  try {
+    const newUser = await prisma.user.create({
+      data: {
+        username: userName,
+        email: email,
+        password: password,
+      },
+    });
 
-  return newUser.id;
+    return newUser.id;
+  } catch (error) {
+    console.log("Error While Creating user", error);
+  }
 }
 
-module.exports = createUser;
+async function verifyUser(userid) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        id: userid,
+      },
+    });
+    return user;
+  } catch (error) {
+    console.log("error while Verifying User");
+    return false;
+  }
+}
+
+async function getUsers(currentUser) {
+  try {
+    const userlist = await prisma.message.findMany({
+      where: {
+        receiverId: currentUser.id,
+      },
+      select: {
+        sender: {
+          select: {
+            id: true,
+            username: true,
+          },
+        },
+      },
+      distinct: ["senderId"],
+    });
+
+    const uniqueUsers = userlist.map((message) => message.sender);
+    return uniqueUsers;
+  } catch (error) {
+    console.log("Error While getting User", error);
+    return false;
+  }
+}
+
+async function searchUser(query) {
+  try {
+    const user = await prisma.user.findFirst({
+      where: {
+        username: query,
+      },
+    });
+
+    return user;
+  } catch (error) {
+    console.log("Error While Searching User", error);
+    return false;
+  }
+}
+
+async function getUser(userName) {
+  try {
+    const user = prisma.user.findUnique({
+      where:{
+        username:userName
+      }
+    })
+
+    return user;
+  } catch (error) {
+    console.log("Error while getting User",error);
+    return false;
+  }
+}
+module.exports = { createUser, verifyUser, getUsers, searchUser, getUser };
